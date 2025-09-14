bb66030783e72d9617ceff0b2aa5cf3543e493f2
Matt Perroni <mattperroni@Matts-MacBook-Air.local>
2025-09-14 14:27:14 -0600

    Styled snippet page

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import { db } from '@/db';
 
@@ -19,8 +20,17 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
     }
 
     return (
-        <div>
-            {snippet.title}
+      <div>
+        <div className="flex m-4 justify-between items-center">
+          <h1 className="text-xl font-bold">{snippet.title}</h1>
+          <div className="flex gap-4">
+            <Link className="border p-2 rounded" href={`/snippets/${snippet.id}/edit`}>Edit</Link>
+            <Link className="border p-2 rounded" href={`/snippets/${snippet.id}/delete`}>Delete</Link>
+          </div>
         </div>
+        <pre className="p-3 border rounded bg-gray-200 border-gray-200">
+          <code>{snippet.code}</code>
+        </pre>
+      </div>
     );
 }
