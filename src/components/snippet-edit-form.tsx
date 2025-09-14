24cccfd0b890dd3395642bb8a0a3142b1175537d
Matt Perroni <mattperroni@Matts-MacBook-Air.local>
2025-09-14 15:18:34 -0600

    Added in codeeditor and hooked up using client component

---
+++ src/components/snippet-edit-form.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import type { Snippet } from "@/generated/prisma";
+import Editor from '@monaco-editor/react';
+
+interface SnippetEditFormProps {
+  snippet: Snippet;
+}
+
+export default function SnippetEditPage({ snippet }: SnippetEditFormProps) {
+  return (
+    <div>
+      <Editor
+        height="40vh"
+        theme="vs-dark"
+        defaultLanguage="javascript"
+        defaultValue={snippet.code}
+        options={{ minimap: { enabled: false } }}
+      />
+    </div>
+  )
+}
